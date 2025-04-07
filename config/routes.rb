ae6d3b941afc2828a0f75074dba95d17717756ad
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-07 14:08:27 +0000

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'   #ここを追記します
-  get 'lists' => 'lists#index'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show'
-  get '/top' => 'homes#top'
-    # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  get 'top' => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
